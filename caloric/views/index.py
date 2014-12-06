d869e7e6f6a76418affd3ee468f74325ebe92083
Marcus McCurdy <marcus.mccurdy@gmail.com>
2014-12-06 17:24:34 -0500

    Adding static assets, package.json

--- caloric/views/index.py
+++ caloric/views/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask.views import MethodView
 
 __author__ = 'Marcus McCurdy'
@@ -9,7 +9,7 @@ index = Blueprint('index', __name__)
 class IndexView(MethodView):
 
     def get(self):
-        return 'hello'
+        return render_template('index.html')
 
 index.add_url_rule('/', view_func=IndexView.as_view('index'))
 
--- manage.py
+++ manage.py
@@ -4,8 +4,9 @@ from flask.ext.script import Manager
 from caloric.app import create_app
 
 app = create_app()
+app.debug = True
 
 manager = Manager(app)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     manager.run()
